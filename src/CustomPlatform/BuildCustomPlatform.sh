090e46c5782dc1707b190504530b2ee7d87662bd
Pavel Krymets <pakrym@microsoft.com>
2016-06-17 10:59:02 -0700

    Custom shared framework and version re-alignment

---
+++ src/CustomPlatform/BuildCustomPlatform.sh
@@ -0,0 +1,38 @@
+﻿#!/bin/bash
+FXVER=1.0.0
+FXNAME=CustomPlatform
+FXOUT=output/$FXNAME/$FXVER
+FXPKG=$FXNAME.$FXVER.nupkg
+FXCOPYTO=$1
+echo "===== Cleanup"
+rm -vrf output
+rm -v ../../local/$FXPKG
+if [[ $FXCOPYTO ]]; then
+	rm -rfv "$FXCOPYTO/$FXNAME"
+fi
+
+
+echo "===== Building framework"
+dotnet restore
+dotnet publish -o $FXOUT
+rm -v $FXOUT/$FXNAME.exe
+rm -v $FXOUT/$FXNAME.dll
+rm -v $FXOUT/$FXNAME.pdb
+rm -v $FXOUT/$FXNAME.runtimeconfig.*
+FXDEPS=$FXOUT/$FXNAME.deps.json
+
+mv $FXDEPS $FXDEPS.old
+head -n -1 $FXDEPS.old > $FXDEPS
+cat Fallbacks.txt >> $FXDEPS
+echo "}" >> $FXDEPS
+rm $FXDEPS.old
+
+echo "===== Building packages"
+dotnet pack -o output
+
+echo "===== Deploying things"
+cp -v output/$FXPKG ../../local
+
+if [[ $FXCOPYTO ]]; then
+	cp -rfv output/$FXNAME "$FXCOPYTO"
+fi
