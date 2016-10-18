9294468e42ee244c831e254a341ed1673c4362ae
Pavel Krymets <pavel@krymets.com>
2016-10-18 14:20:49 -0700

    Use Urls

--- src/Compression/Program.cs
+++ src/Compression/Program.cs
@@ -14,6 +14,7 @@ namespace Compression
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseUrls("http://*:5000")
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
