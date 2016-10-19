d0f7ff1e277b33ee464e8364b8efadf00b06ec80
Pavel Krymets <pavel@krymets.com>
2016-10-19 14:32:40 -0700

    Diagnostics

---
+++ src/Diagnostics/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Diagnostics.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            GetValue();
+            return View();
+        }
+
+        private static void GetValue()
+        {
+            GetValue1();
+        }
+
+        private static void GetValue1()
+        {
+            try
+            {
+                throw new IOException("aaaa");
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Exception", ex);
+            }
+        }
+    }
+}
